6390564b98e108df89a9eede12e9dd989675d1d4
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2025-01-02 11:09:57 +0200

    Add Link in logo

--- src/app/(admin)/companies/[id]/new-promotion/page.tsx
+++ src/app/(admin)/companies/[id]/new-promotion/page.tsx
@@ -3,12 +3,6 @@
 import { use } from 'react';
 import PromotionForm from '@/app/components/promotion-form';
 
-export interface INewPromotionPageProps {
-  params: {
-    id: string;
-  };
-}
-
 const NewPromotionPage = ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = use(params);
 
